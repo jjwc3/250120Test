ce7f344e9166a1a2be1fe42e8795a39acf03111a
jjwc3 <155734556+jjwc3@users.noreply.github.com>
2025-01-23 23:39:19 +0900

    250123

--- app/src/main/java/com/example/a250120test/MainActivity.kt
+++ app/src/main/java/com/example/a250120test/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.InteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,61 +22,35 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TextFieldDefaults.indicatorLine
 import androidx.compose.material3.TimeInput
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.a250120test.ui.theme.Typography
-import com.example.a250120test.ui.theme.npsFont
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -96,14 +67,14 @@ class MainActivity : ComponentActivity() {
                     Main(
                         modifier = Modifier
                             .background(color = Color(0xFFF6F6F8)),
-                        onNavigateToDrawing = {navController.navigate("Drawing")}
+//                        onNavigateToDrawing = {navController.navigate("Drawing")}
                     )
                 }
                 composable("Drawing") {
                     Drawing(
-                        modifier = Modifier
-                            .background(color = Color(0xFFF6F6F8)),
-                        onNavigateToMain = {navController.navigate("Drawing")}
+//                        modifier = Modifier
+//                            .background(color = Color(0xFFF6F6F8)),
+//                        onNavigateToMain = {navController.navigate("Drawing")}
                     )
                 }
             }
@@ -117,7 +88,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Main(
     modifier: Modifier = Modifier,
-    onNavigateToDrawing : () -> Unit,
+//    onNavigateToDrawing : () -> Unit,
 //    viewModel: MainViewModel,
 ) {
     Scaffold (
@@ -179,7 +150,7 @@ fun Main(
                         modifier = Modifier,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        var dateList: Array<String> = arrayOf("월", "화", "수", "목", "금", "토", "일")
+                        val dateList: Array<String> = arrayOf("월", "화", "수", "목", "금", "토", "일")
                         for (i in dateList)
                             CircularButton(
                                 text = i,
@@ -291,8 +262,8 @@ fun Main(
 
 @Composable
 fun Drawing(
-    modifier: Modifier = Modifier,
-    onNavigateToMain: () -> Unit,
+//    modifier: Modifier = Modifier,
+//    onNavigateToMain: () -> Unit,
 ) {}
 
 @Composable
